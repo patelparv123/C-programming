7115e5b9112a282801619351a4d0cf16f4cd8620
patelparv123 <patelparv135@gmail.com>
2025-09-02 08:24:25 +0530

    Add files via upload

---
+++ sum and mean of any 10 values.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, num , sum = 0;
+    float mean;
+
+    printf("Enter 10 numbers:\n");
+
+    for(i = 1; i <= 10; i++) {
+        printf("Number %d: ", i);
+        scanf("%d", &num);
+        sum += num;  
+    }
+    mean = sum / 10.0;
+
+    printf("Sum = %d\n", sum);
+    printf("Mean = %f\n", mean);
+
+    return 0;
+}
